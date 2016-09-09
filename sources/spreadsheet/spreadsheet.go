eae0095a56a63e66a14d74806172dd244ac1c351
Umar Muneer <umar.muneer@careaxiom.com>
2016-09-09 07:25:30 +0500

    added reading from spreadsheet

--- sources/spreadsheet/spreadsheet.go
+++ sources/spreadsheet/spreadsheet.go
@@ -3,23 +3,37 @@ package spreadsheet
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"whats-for-lunch/authentication"
 )
 
-/*Menu representation of a menu for a day*/
-type Menu struct {
-	New string
-	Old string
+var apiURL = "https://sheets.googleapis.com/v4/spreadsheets"
+
+/*Output representation of a menu for a day*/
+type Output struct {
+	New     string
+	Old     string
+	Dessert string
 }
 
 /*GetTomorrowsMenu returns tomorrow's menu*/
 func GetTomorrowsMenu(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("getting tomorrow's menu")
-	_, err := authentication.GetClient()
+	spreadSheetClient, err := authentication.GetClient()
 	if err != nil {
 		fmt.Println(err)
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
-	json.NewEncoder(res).Encode(Menu{New: "Biryani", Old: "Karahi"})
+	var oldURL = apiURL + "/" + os.Getenv("OLD_MENU_SPREADSHEET_ID") + "/values" + "/A6:E36"
+	fmt.Println("spreadsheet url is", oldURL)
+	spreadSheetResponse, spreadSheetErr := spreadSheetClient.Get(oldURL)
+	if spreadSheetErr != nil {
+		fmt.Println("error reading from spreadsheet", spreadSheetErr.Error())
+		http.Error(res, spreadSheetErr.Error(), http.StatusInternalServerError)
+	}
+	output, _ := ioutil.ReadAll(spreadSheetResponse.Body)
+	fmt.Println(string(output[:]), "yayayaya")
+	json.NewEncoder(res).Encode(Output{New: "Biryani", Old: "Karahi", Dessert: "Custard"})
 }
