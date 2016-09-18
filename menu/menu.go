0423a9a6fc8a0d068c07eba76f60c8e9123cb129
Umar Muneer <umar.muneer@careaxiom.com>
2016-09-18 20:23:08 +0500

    corrected residual errors

--- menu/menu.go
+++ menu/menu.go
@@ -2,6 +2,7 @@ package menu
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -16,15 +17,24 @@ type Entry struct {
 	Dessert       string
 }
 
-func (entry *Entry) newEntry(data []string) *Entry {
+func newEntry(data []string) *Entry {
 	var dessert = ""
+	var secondaryDish = ""
+	var mainDish = ""
+
+	if len(data) > 2 {
+		mainDish = data[2]
+	}
+	if len(data) > 3 {
+		secondaryDish = data[3]
+	}
 	if len(data) > 4 {
 		dessert = data[4]
 	}
 
 	return &Entry{
-		MainDish:      data[2],
-		SecondaryDish: data[3],
+		MainDish:      mainDish,
+		SecondaryDish: secondaryDish,
 		Dessert:       dessert,
 	}
 }
@@ -52,16 +62,19 @@ func New(client *http.Client, sheetID string, sheetOffset int) *SpreadSheetMenu 
 }
 
 /*GetMenuEntry gets a menu entry for a given date
-  date required date in the format DD/MM/YYYY, start of day in Asia/Karachi timezone
+  date required date in the format MMM/DD/YYYY, start of day in Asia/Karachi timezone
 */
 func (menu SpreadSheetMenu) GetMenuEntry(date string) (*Entry, error) {
-	dayTime, err := time.Parse("01/01/2006", date)
+	dayTime, err := time.Parse("02/01/2006", date)
+	fmt.Println("requested day time is", dayTime)
 	if err != nil {
 		return new(Entry), err
 	}
 	column := strconv.Itoa(menu.sheetOffset + dayTime.Day())
 	cellRange := "A" + string(column) + ":" + "E" + string(column)
 	url := os.Getenv("SHEETS_API_URL") + "/" + menu.sheetID + "/values/" + cellRange
+	fmt.Println("day of month is", dayTime.Day())
+	fmt.Println("url to get menu is -> ", url)
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("Accept", "application/json")
 	response, responseErr := menu.client.Do(req)
@@ -77,9 +90,6 @@ func (menu SpreadSheetMenu) GetMenuEntry(date string) (*Entry, error) {
 	if marshalError != nil {
 		return new(Entry), marshalError
 	}
-	menuEntry := &Entry{
-		MainDish: spreadSheetOutput.Values[0][2],
-	}
-
+	menuEntry := newEntry(spreadSheetOutput.Values[0])
 	return menuEntry, nil
 }
